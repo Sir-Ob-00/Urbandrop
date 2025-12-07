eb9407097d8092fb193d7a3a76bebef77c99cf86
Owusu_Boateng <owusuboat05@gmail.com>
2025-12-07 17:01:38 +0000

    touches

--- src/components/home/EthnicMosaic.jsx
+++ src/components/home/EthnicMosaic.jsx
@@ -13,17 +13,26 @@ import GhanaFlag from '../../assets/images/home/Ghana/Flag_of_Ghana.svg.png';
 import GhanaImg1 from '../../assets/images/home/Ghana/GB-ABEMUDRO-CREAM-800g-X-12.jpg';
 import GhanaImg2 from '../../assets/images/home/Ghana/Gino-Tomato-Puree-210g.jpg';
 import GhanaImg3 from '../../assets/images/home/Ghana/Titus Sardine in Soybean & Chilli Oil.jpg';
+import GhanaImg4 from '../../assets/images/home/grocery-1.jpg';
+import GhanaImg5 from '../../assets/images/home/grocery-2.jpg';
+import GhanaImg6 from '../../assets/images/home/grocery-13.jpg'; 
+
 
 import IndiaFlag from '../../assets/images/home/India/Flag_of_India.png';
 import IndiaImg1 from '../../assets/images/home/India/NIRU-MILLET_400x.png';
 import IndiaImg2 from '../../assets/images/home/India/TRSChieckPeas.jpg';
 import IndiaImg3 from '../../assets/images/home/India/TRSUridDal.jpg';
+import IndiaImg4 from '../../assets/images/home/grocery-3.jpg';
+import IndiaImg5 from '../../assets/images/home/grocery-4.jpg';
+import IndiaImg6 from '../../assets/images/home/grocery-14.jpg';
 
 import JamaicaFlag from '../../assets/images/home/Jamaica/Flag_of_Jamaica.svg.png';
 import JamaicaImg1 from '../../assets/images/home/Jamaica/BANANA-LEAF_400x.jpg';
 import JamaicaImg2 from '../../assets/images/home/Jamaica/De Rica Tomato Paste 400g.jpg';
 import JamaicaImg3 from '../../assets/images/home/Jamaica/Gongura-leaves_ced44f2f-8218-464a-b73e-f30992d4994d_400x.jpg';
 import JamaicaImg4 from '../../assets/images/home/Jamaica/TROPICWAY-PEANUT-BUTTER-12×500G.jpg';
+import JamaicaImg5 from '../../assets/images/home/grocery-5.jpg';
+import JamaicaImg6 from '../../assets/images/home/grocery-15.jpg';
 
 import KenyaFlag from '../../assets/images/home/Kenya/Flag_of_Kenya.svg.png';
 import KenyaImg1 from '../../assets/images/home/Kenya/JACKFRUITPEALED300G_APPROX5PCS_400x.png';
@@ -37,17 +46,21 @@ import MoroccoImg1 from '../../assets/images/home/Morocco/41YDq-tchUL_280x.png';
 import MoroccoImg2 from '../../assets/images/home/Morocco/big_5022496019750_280x.png';
 import MoroccoImg3 from '../../assets/images/home/Morocco/IDHAYAM-OIL-1LTR.png';
 import MoroccoImg4 from '../../assets/images/home/Morocco/SRIDURGACOLDPRESSEDGINGELLY_SESAME_OIL1LTR_280x.png';
+import MoroccoImg5 from '../../assets/images/home/grocery-6.jpg';
 
 import NigeriaFlag from '../../assets/images/home/Nigeria/Flag_of_Nigeria.svg.webp';
 import NigeriaImg1 from '../../assets/images/home/Nigeria/elex Nigerian Roasted peanuts.jpg';
 import NigeriaImg2 from '../../assets/images/home/Nigeria/Niyis Peeled Beans 2kg.jpg';
 import NigeriaImg3 from '../../assets/images/home/Nigeria/Olu Olu Plantain chips Green.jpg';
 import NigeriaImg4 from '../../assets/images/home/Nigeria/Olu-Olu-Poundo-Iyan-1.2kg--460x575.jpg';
+import NigeriaImg5 from '../../assets/images/home/grocery-7.jpg';
 
 import PakistanFlag from '../../assets/images/home/Pakistan/Flag_of_Pakistan.svg.png';
 import PakistanImg1 from '../../assets/images/home/Pakistan/BRAHMINS_PUTTU_PODI_1KG_-_WHITE_280x.png';
 import PakistanImg2 from '../../assets/images/home/Pakistan/HEERAGRAMFLOUR1KG_280x.png';
 import PakistanImg3 from '../../assets/images/home/Pakistan/NAGAIDIYAPPAMFLOUR500G_280x.png';
+import PakistanImg4 from '../../assets/images/home/grocery-8.jpg';
+import PakistanImg5 from '../../assets/images/home/grocery-9.jpg';
 
 import SenegalFlag from '../../assets/images/home/Senegal/Flag_of_Senegal.svg.webp';
 import SenegalImg1 from '../../assets/images/home/Senegal/819D8piBVKL._UF1000_1000_QL80.jpg';
@@ -66,12 +79,16 @@ import SomaliaImg5 from '../../assets/images/home/Somalia/SAKTHI-FISH-FRY-MASALA
 import SouthAfricaFlag from '../../assets/images/home/South_Africa/Flag_of_South_Africa.svg.png';
 import SouthAfricaImg1 from '../../assets/images/home/South_Africa/Copy of nestle-milo-singapore-1.8kgs.jpg';
 import SouthAfricaImg2 from '../../assets/images/home/South_Africa/HALDIRAMSFROZENMIXVEGPARATHA400G_400x.png';
-import SouthAfricaImg3 from '../../assets/images/home/South_Africa/Uncle-Johns-Bread-800g.jpg';
+import SouthAfricaImg3 from '../../assets/images/home/South_Africa/Uncle-Johns-Bread-800g.jpg'; 
+import SouthAfricaImg4 from '../../assets/images/home/grocery-9.jpg';
+import SouthAfricaImg5 from '../../assets/images/home/grocery-10.jpg';
 
 import ZimbabweFlag from '../../assets/images/home/Zimbabwe/Flag_of_Zimbabwe.svg.png';
 import ZimbabweImg1 from '../../assets/images/home/Zimbabwe/GB-PEANUT-BUTTER-NO-SUGAR-400G-X-12.jpg';
 import ZimbabweImg2 from '../../assets/images/home/Zimbabwe/golden-morn-maize-cereal-with-soya-protein-300g.jpg';
 import ZimbabweImg3 from '../../assets/images/home/Zimbabwe/SRIDURGARAGIFLOUR1KG_400x.png';
+import ZimbabweImg4 from '../../assets/images/home/grocery-11.jpg';
+import ZimbabweImg5 from '../../assets/images/home/grocery-12.jpg';
 
 const countries = [
   {
@@ -82,17 +99,17 @@ const countries = [
   {
     name: 'Ghana',
     flag: GhanaFlag,
-    groceries: [GhanaImg1, GhanaImg2, GhanaImg3]
+    groceries: [GhanaImg1, GhanaImg2, GhanaImg3, GhanaImg4, GhanaImg5, GhanaImg6]
   },
   {
     name: 'India',
     flag: IndiaFlag,
-    groceries: [IndiaImg1, IndiaImg2, IndiaImg3]
+    groceries: [IndiaImg1, IndiaImg2, IndiaImg3, IndiaImg4, IndiaImg5, IndiaImg6]
   },
   {
     name: 'Jamaica',
     flag: JamaicaFlag,
-    groceries: [JamaicaImg1, JamaicaImg2, JamaicaImg3, JamaicaImg4]
+    groceries: [JamaicaImg1, JamaicaImg2, JamaicaImg3, JamaicaImg4, JamaicaImg5, JamaicaImg6]
   },
   {
     name: 'Kenya',
@@ -102,17 +119,17 @@ const countries = [
   {
     name: 'Morocco',
     flag: MoroccoFlag,
-    groceries: [MoroccoImg1, MoroccoImg2, MoroccoImg3, MoroccoImg4]
+    groceries: [MoroccoImg1, MoroccoImg2, MoroccoImg3, MoroccoImg4, MoroccoImg5]
   },
   {
     name: 'Nigeria',
     flag: NigeriaFlag,
-    groceries: [NigeriaImg1, NigeriaImg2, NigeriaImg3, NigeriaImg4]
+    groceries: [NigeriaImg1, NigeriaImg2, NigeriaImg3, NigeriaImg4, NigeriaImg5]
   },
   {
     name: 'Pakistan',
     flag: PakistanFlag,
-    groceries: [PakistanImg1, PakistanImg2, PakistanImg3]
+    groceries: [PakistanImg1, PakistanImg2, PakistanImg3, PakistanImg4, PakistanImg5]
   },
   {
     name: 'Senegal',
@@ -127,12 +144,12 @@ const countries = [
   {
     name: 'South Africa',
     flag: SouthAfricaFlag,
-    groceries: [SouthAfricaImg1, SouthAfricaImg2, SouthAfricaImg3]
+    groceries: [SouthAfricaImg1, SouthAfricaImg2, SouthAfricaImg3, SouthAfricaImg4, SouthAfricaImg5]
   },
   {
     name: 'Zimbabwe',
     flag: ZimbabweFlag,
-    groceries: [ZimbabweImg1, ZimbabweImg2, ZimbabweImg3]
+    groceries: [ZimbabweImg1, ZimbabweImg2, ZimbabweImg3, ZimbabweImg4, ZimbabweImg5]
   }
 ];
 
